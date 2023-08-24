841b31b155776ddc8972cf5039d310e1224722ce
sammyalwayscode <olorundasamuel2@gmail.com>
2023-08-24 09:45:41 +0100

    Arrays and conditional statements

---
+++ ConditionalStatements.java
@@ -0,0 +1,5 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JavaArrays.java
@@ -0,0 +1,9 @@
+public class JavaArrays {
+    public static void main(String[] args) {
+        String[] myFriends = { "Ayokunle", "Ayomide", "Esther" };
+        myFriends[0] = "Sylvia";
+
+        System.out.println(myFriends[0]);
+        System.out.println(myFriends.length);
+    }
+}
