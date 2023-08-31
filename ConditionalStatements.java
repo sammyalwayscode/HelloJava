9f41a809269e23147faadcc3089e21924600fac9
sammyalwayscode <olorundasamuel2@gmail.com>
2023-08-31 12:43:38 +0100

    Conditional Statements

--- ConditionalStatements.java
+++ ConditionalStatements.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ConditionalStatements {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter valueOne");
+        int valueOne = userInput.nextInt();
+        System.out.println("Enter valueTwo:");
+        int valueTwo = userInput.nextInt();
 
+        if (valueOne > valueTwo) {
+            System.out.println(valueOne);
+        } else {
+            System.out.println(valueTwo);
+        }
     }
 }
