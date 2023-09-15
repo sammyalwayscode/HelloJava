0f9c411ab5f28a9c5e9c025f43b6006ec3cd43d4
sammyalwayscode <olorundasamuel2@gmail.com>
2023-09-15 13:32:01 +0100

    Java Loops

---
+++ LoopInJava.java
@@ -0,0 +1,16 @@
+public class LoopInJava {
+    public static void main(String[] args) {
+        // While Loop
+        int index = 1;
+        while (index <= 10) {
+            System.out.println(index);
+            index++;
+        }
+
+        // For Loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
